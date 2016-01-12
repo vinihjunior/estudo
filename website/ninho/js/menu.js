12848bd15a8f1cf8a0a6f6cf20208304fc0673d7
vinihjunior <vinicius.dphelippe@gmail.com>
2016-01-12 21:50:21 -0200

    push completo

---
+++ website/ninho/js/menu.js
@@ -0,0 +1,45 @@
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+};
+
+
+$(document).ready(main);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$('a').click(function(){
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1250);
+    return false;
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
